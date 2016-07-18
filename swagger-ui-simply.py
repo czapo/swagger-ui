fa8e4edaa558efcf6c5481cbea0547b2d796005c
knifeofdreams <judit.ordog-andrasi@prezi.com>
2016-07-18 16:59:40 +0200

    imports fixed

--- swagger-ui-simply.py
+++ swagger-ui-simply.py
@@ -1,7 +1,10 @@
-from projects.operations import NPMOperations
+# from projects.operations import NPMOperations
+from projects import operations
+
+print dir(operations)
 
 
-class Project(NPMOperations):
+class Project(operations.NPMOperations):
 
     def get_project_name(self):
         return 'swaggerui'
